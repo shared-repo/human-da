885359d5b3d064a9704fa725b0e5a80e5eb661ab
howlab2025 <howlab2025@gmail.com>
2025-01-09 11:48:33 +0900

    update code

--- workspace/webapps/demoweb/__init__.py
+++ workspace/webapps/demoweb/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from .views import main_view
+from .views import main_view, auth_view
 
 def create_app():
 
@@ -10,6 +10,7 @@ def create_app():
     #     return render_template('index.html') # templates/index.html을 처리해서 응답
 
     app.register_blueprint(main_view.main_bp)
+    app.register_blueprint(auth_view.auth_bp)
 
 
     return app
---
+++ workspace/webapps/demoweb/views/auth_view.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+auth_bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@auth_bp.route("/register/")
+def register():
+    return render_template("auth/register.html")
+
+@auth_bp.route("/login/")
+def login():
+    return render_template("auth/login.html")
