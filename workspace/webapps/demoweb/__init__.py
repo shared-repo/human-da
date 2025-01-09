cbaff6409fdf072d017d21d910c0f634e8ca76db
howlab2025 <howlab2025@gmail.com>
2025-01-09 09:54:20 +0900

    update code

--- workspace/webapps/demoweb/__init__.py
+++ workspace/webapps/demoweb/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
+from .views import main_view
 
 def create_app():
 
     app = Flask(__name__) # webapp 만들기
 
-    @app.route("/")
-    def index(): 
-        return render_template('index.html') # templates/index.html을 처리해서 응답
+    # @app.route("/")
+    # def index(): 
+    #     return render_template('index.html') # templates/index.html을 처리해서 응답
+
+    app.register_blueprint(main_view.main_bp)
+
 
     return app
