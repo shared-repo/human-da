1a01b0a5c40b7d3a40d34c61477bc7a466534103
howlab2025 <howlab2025@gmail.com>
2025-02-12 10:26:04 +0900

    update code

--- workspace/webapps/chatbotweb/views/chatbot_view.py
+++ workspace/webapps/chatbotweb/views/chatbot_view.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify, session
 from openai import OpenAI
 import os
+import shutil
 
 chatbot_bp = Blueprint("chatbot", __name__, url_prefix="/chatbot")
 
@@ -70,6 +71,83 @@ def handle_chat_text_with_history():
     return jsonify({ 'message' : completion.choices[0].message.content })
 
 
+# 요청 처리 5-1 : openai api service 사용 (대화 history 사용 + 추가 정보 사용)
+@chatbot_bp.route("/chat-text-with-custom-info/", methods=['POST'])
+def handle_chat_text_with_custom_info():
+    json_data = request.get_json()
+    message = json_data.get('message')
+
+    system_message = """
+             당신은 모든 정보를 잘 알고 있는 친절한 안내자입니다. 
+             당신은 질문에 대해 가능한 간결하게 답변해야 합니다.
+             그리고 아래의 고려사항을 반영해서 답변을 만들어야 합니다.
+
+             [고려사항]
+             사용자는 남성이며 매운음식을 좋아합니다.
+             사용자는 민트초코 맛을 매우 좋아합니다.
+             사용자는 국물있는 음식도 좋아합니다.
+             사용자는 콩국수는 매우 싫어합니다."""
+
+    chat_history = session.get('chat-history', [])
+    if len(chat_history) == 0: # if chat_history:
+        chat_history.append({ "role": "system", "content": system_message })
+
+    chat_history.append({ "role": "user", "content": message })
+
+    completion = client.chat.completions.create(
+        model="chatgpt-4o-latest",
+        messages=chat_history,
+        n=1,
+        temperature=1
+    )
+    
+    chat_history.append({ "role": "assistant", "content": completion.choices[0].message.content })
+
+    session['chat-history'] = chat_history
+
+    return jsonify({ 'message' : completion.choices[0].message.content })
+
+# 요청 처리 5-2 : openai api service 사용 (대화 history 사용 + 추가 정보 사용)
+@chatbot_bp.route("/chat-text-with-custom-info2/", methods=['POST'])
+def handle_chat_text_with_history2():
+    json_data = request.get_json()
+    message = json_data.get('message')
+
+    prompt = f"""
+             아래의 고려사항을 적극적으로 반영해서 질문에 대한 답변을 만들어 주세요.
+
+             ###고려사항###
+             1. 사용자는 남성이며 매운음식을 좋아합니다.
+             2. 사용자는 민트초코 맛을 매우 좋아합니다.
+             3. 사용자는 국물있는 음식도 좋아합니다.
+             4. 사용자는 콩국수는 매우 싫어합니다.
+
+             질문 : {message}
+             답변 : """
+
+    chat_history = session.get('chat-history', [])
+    if len(chat_history) == 0: # if chat_history:
+        chat_history.append({ "role": "system", "content": "당신은 모든 정보를 잘 알고 있는 친절한 안내자입니다. 질문에 대해 가능한 간결하게 답변해야 합니다." })
+
+    chat_history.append({ "role": "user", "content": prompt })
+
+    completion = client.chat.completions.create(
+        model="chatgpt-4o-latest",
+        messages=chat_history,
+        n=1,
+        temperature=1
+    )
+
+    last_chat = chat_history[-1]
+    last_chat['content'] = message
+    
+    chat_history.append({ "role": "assistant", "content": completion.choices[0].message.content })
+
+    session['chat-history'] = chat_history
+
+    return jsonify({ 'message' : completion.choices[0].message.content })
+
+
 @chatbot_bp.route("/reload-chat-history/")
 def reload_chat_history():
     chat_history = session.get("chat-history", [])
@@ -79,3 +157,17 @@ def reload_chat_history():
     session['chat-history'] = chat_history
 
     return jsonify( chat_history[1:] if len(chat_history) > 1 else [] )
+
+
+@chatbot_bp.route("/audio-to-text/", methods=['POST'])
+def audio_to_text():
+    audio = request.files.get("audio")
+    audio.save(audio.filename)
+
+    with open(audio.filename, 'rb') as f:
+        transcription = client.audio.transcriptions.create(
+            model="whisper-1",
+            file=f
+        )
+    print(transcription.text)
+    return transcription.text
