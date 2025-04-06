0f15f2115de8f777b3bdc1f76ea473398bb5eb18
ml.dl.instructor <ml.dl.instructor@gmail.com>
2025-04-06 21:26:27 +0900

    update code

--- workspace/webapps/demoweb/db_utils/data_util.py
+++ workspace/webapps/demoweb/db_utils/data_util.py
@@ -4,15 +4,33 @@ import pymysql
 def select_all_quotes():
     conn = pymysql.connect(
         host='localhost',
-        user='humanda5',
-        password='humanda5',
-        db='humanda5_db'
+        user='developer',
+        password='developer',
+        db='demoweb'
     )
 
     try:
         with conn.cursor() as cursor:
             sql = "SELECT code, date, open, high, low, close, volume FROM stock_data limit 10"
             cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
+    finally:
+        conn.close()
+
+
+def select_price_data(code, start_date, end_date):
+    conn = pymysql.connect(
+        host='localhost',
+        user='developer',
+        password='developer',
+        db='demoweb'
+    )
+
+    try:
+        with conn.cursor() as cursor:
+            sql = "SELECT code, date, open, high, low, close, volume FROM stock_data where code=%s and date >= %s and date <= %s"
+            cursor.execute(sql, (code, start_date, end_date))
             result = cursor.fetchall()
             return result
     finally:
--- workspace/webapps/demoweb/views/data_view.py
+++ workspace/webapps/demoweb/views/data_view.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, request, jsonify, session, current_app
 
+import numpy as np
+import pandas as pd
+
+
 from ..db_utils import data_util
 
 data_bp = Blueprint("data", __name__, url_prefix="/data")
@@ -13,4 +17,20 @@ def index():
 def get_list():
     quotes = data_util.select_all_quotes()       
     return jsonify(quotes), 200
+
+
+@data_bp.route("/get-price-data/", methods=["GET"])
+def get_price_data():
+    code = request.args.get("code")
+    start_date = request.args.get("start_date")
+    end_date = request.args.get("end_date")
+    quotes = data_util.select_price_data(code, start_date, end_date)
+    quotes_ar = np.array(quotes)
+    quotes_dict = {}
+    for idx, col in enumerate(['code', 'date', 'open', 'high', 'low', 'close', 'volume']):
+        if col == 'date':
+            quotes_dict[col] = [ str(d)[:10] for d in quotes_ar[:, idx]]
+        else:
+            quotes_dict[col] = quotes_ar[:, idx].tolist()
+    return jsonify(quotes_dict), 200
 
